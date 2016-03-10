cc44fd8aa4e08fd944452faca0fd3a0040422206
Nabeel Mahmood <Nabeel Mahmood>
2016-03-10 19:43:31 +0500

    initial commit

---
+++ factorial.go
@@ -0,0 +1,16 @@
+//Package factorial shows the factorial of a given number
+package factorial
+
+import (
+    "math/big"
+)
+
+//Function factorial will give the output of number's facotrila
+func factorial(x *big.Int) *big.Int {
+    n := big.NewInt(1)
+    if x.Cmp(big.NewInt(0)) == 0 {
+        return n
+    }
+    return n.Mul(x, factorial(n.Sub(x, n)))
+}
+
